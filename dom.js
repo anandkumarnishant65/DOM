37efcd3913081b1c6c5c09d8073934d341bdc377
anandkumarnishant65 <anandkumarnishant65@gmail.com>
2023-08-06 14:05:20 +0530

    2nd commit

--- dom.js
+++ dom.js
@@ -1,4 +1,19 @@
 //console.dir(document);
-var headerTitle=document.getElementById('header-title');
-var header=document.getElementById('main-header');
-header.style.borderBottom = 'solid 3px #000';
+//var headerTitle=document.getElementById('header-title');
+//console.log(headerTitle);
+//headerTitle.textContent='Hello';
+
+//var header=document.getElementById('main-header');
+//header.style.borderBottom = 'solid 3px #000';
+
+//console.log(document.getElementById('header-title'));
+
+// Get element by class name
+
+var item=document.getElementsByClassName('list-group-item');
+console.log(item[2]);
+item[2].style.backgroundColor = 'green';
+
+for(var i=0; i<item.length;i++){
+    item[i].style.fontWeight = 'bold';
+}
