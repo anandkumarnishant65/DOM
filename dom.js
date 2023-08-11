873f31d374361c074febebcfbe3084f5290a9a16
anandkumarnishant65 <anandkumarnishant65@gmail.com>
2023-08-11 21:19:06 +0530

    NodeCreation

--- dom.js
+++ dom.js
@@ -30,19 +30,63 @@
 // }
 
 // QUERY SELECTOR //
-var seconditem = document.querySelector('.list-group-item:nth-child(2)');
-seconditem.style.backgroundColor = 'pink';
-var thirditem = document.querySelector('.list-group-item:nth-child(3)');
-thirditem.style.color = 'green';
+// var seconditem = document.querySelector('.list-group-item:nth-child(2)');
+// seconditem.style.backgroundColor = 'pink';
+// var thirditem = document.querySelector('.list-group-item:nth-child(3)');
+// thirditem.style.color = 'green';
 
-// QUERY SELECTORALL //
-var odd = document.querySelectorAll('li:nth-child(odd)');
-for(var i=0;i<odd.length;i++){
-    odd[i].style.backgroundColor = 'green';
-    odd[1].style.color = 'green';
-}
-var even = document.querySelectorAll('li:nth-child(even)');
-for(var i=0;i<even.length;i++){
-    even[0].style.color = 'green';
+// // QUERY SELECTORALL //
+// var odd = document.querySelectorAll('li:nth-child(odd)');
+// for(var i=0;i<odd.length;i++){
+//     odd[i].style.backgroundColor = 'green';
+//     odd[1].style.color = 'green';
+// }
+// var even = document.querySelectorAll('li:nth-child(even)');
+// for(var i=0;i<even.length;i++){
+//     even[0].style.color = 'green';
 
-}
+// }
+
+// TRAVERSING DOM //
+
+// var itemList = document.querySelector('#items');
+// console.log(itemList.parentNode);
+// //itemList.parentNode.style.backgroundColor = '#e1e1e1';
+
+// //PARENTELEMENT//
+// //itemList.parentNode.style.backgroundColor = 'red';
+
+// //CHILDNODE//
+// itemList.children[1].style.backgroundColor = 'pink';
+
+// //FIRSTELEMENTCHILD//
+// itemList.firstElementChild.style.backgroundColor = 'yellow';
+
+// //LASTCHILD
+// itemList.lastElementChild.style.backgroundColor = 'blue';
+
+// //nextSibling
+// //console.log(itemList.nextSibling);
+// //console.log(itemList.nextElementSibling);
+
+// //previousSibling//
+// itemList.previousElementSibling.style.color = 'red';
+
+//CreateElement//
+//creat a Div//
+var newDiv = document.createElement('div','/div');
+//Add class//
+newDiv.className = 'hello';
+//Add Id//
+newDiv.id='hello1';
+//add attribute//
+newDiv.setAttribute('title' , 'hello div');
+//create textNode//
+var newDivText = document.createTextNode('HEllo');
+//Add text to Div//
+newDiv.appendChild(newDivText);
+var container = document.querySelector('header .container');
+var h1 = document.querySelector('header h1');
+container.insertBefore(newDiv,h1);
+
+console.log(newDiv);
